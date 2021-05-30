03fda21dc3471b6181c57b1c8b5e691bdb3f4806
l3ns <peplosv@gmail.com>
2021-05-30 16:27:17 +0500

    Created templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Привет, мир'
-
+    user = {'username': 'p3plos'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Главная', user=user,
+                           posts=posts)
